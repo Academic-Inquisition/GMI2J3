5a67208b433e185196b8a91a36733ce5df350783
Lanse505 <simon_stalnabb@hotmail.com>
2023-04-25 19:45:45 +0200

    Push starts of new tests

---
+++ lab2/2.3/RomanNumeralStud.Tests/RomanOperatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RomanNumeralStud.Tests
+{
+    [TestClass]
+    public class RomanOperatorTests
+    {
+        [TestMethod]
+        public void TestPlusOperator()
+        {
+
+        }
+    }
+}
